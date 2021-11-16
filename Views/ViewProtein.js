04b47f6e52ef09f32fbbb5a09397641133ff695f
nhakkaou <noureddine.hak@gmail.com>
2021-11-16 19:06:45 +0100

    Fix Distance between Atoms

--- Views/ViewProtein.js
+++ Views/ViewProtein.js
@@ -4,7 +4,7 @@ import * as THREE from "three";
 import matchAll from "string.prototype.matchall";
 import React, { useEffect, useState } from "react";
 import { View, Text, SafeAreaView, Dimensions } from "react-native";
-import { useHistory } from "react-router-native";
+// import { useHistory } from "react-router-native";
 import MaterialCommunityIcons from "react-native-vector-icons/MaterialCommunityIcons";
 import Axios from "axios";
 import Colors from "./CPK_Colors.json";
@@ -13,99 +13,44 @@ import parsePdb from "parse-pdb";
 import OrbitControlsView from "expo-three-orbit-controls";
 
 const ViewProtein = (props) => {
-  const history = useHistory();
-  const [connects, setConnects] = useState([
-    ["1", "2", "3"],
-    ["2", "1"],
-    ["3", "1"],
-  ]);
+  // const history = useHistory();
+  const [connects, setConnects] = useState([]);
   const [mount, setMounted] = useState(true);
   const [Atoms, setAtoms] = useState([]);
-  const { ligand } = props.location;
+  // const { ligand } = props.location;
+  const ligand = "011";
   const url1 = `https://files.rcsb.org/ligands/view/${ligand}_model.pdb`;
   const url2 =
     "https://files.rcsb.org/ligands/0/" + ligand + "/" + ligand + "_ideal.pdb";
   const [width, setWidth] = useState(Dimensions.get("screen").width);
-  const [height, setHeight] = useState(Dimensions.get("screen").height);
-  const str = [
-    {
-      altLoc: "",
-      chainID: "A",
-      charge: "2",
-      element: "O",
-      iCode: "",
-      name: "C1",
-      occupancy: NaN,
-      resName: "0E5",
-      resSeq: 1,
-      serial: 1,
-      tempFactor: NaN,
-      x: 0,
-      y: 2,
-      z: 10,
-    },
-    {
-      altLoc: "",
-      chainID: "A",
-      charge: "2",
-      element: "H",
-      iCode: "",
-      name: "C1",
-      occupancy: NaN,
-      resName: "0E5",
-      resSeq: 1,
-      serial: 1,
-      tempFactor: NaN,
-      x: 0,
-      y: -2,
-      z: 5,
-    },
-    {
-      altLoc: "",
-      chainID: "A",
-      charge: "2",
-      element: "H",
-      iCode: "",
-      name: "C1",
-      occupancy: NaN,
-      resName: "0E5",
-      resSeq: 1,
-      serial: 1,
-      tempFactor: NaN,
-      x: 0,
-      y: 4,
-      z: 5,
-    },
-  ];
+  const [height, setHeight] = useState(Dimensions.get("screen").height - 10);
 
   useEffect(() => {
-    Axios(url2)
+    console.log("WIDTH", width / 2);
+    Axios(url1)
       .then((res) => {
-        console.log("data", res.data);
         if (res.data) {
-          const array = [...matchAll(res.data, /^CONECT(:?\s*\d+.+)+/gm)];
-          console.log(getConnect(array));
-          setAtoms(parsePdb(res.data));
+          let array = [...matchAll(res.data, /^CONECT(:?\s*\d+.+)+/gm)];
+          let atomsPdb = parsePdb(res.data);
+          console.log(res.data);
+          console.table(atomsPdb.atoms);
+          setAtoms(atomsPdb);
+          array = array.filter((el, key) => key < atomsPdb.atoms?.length);
           setConnects(getConnect(array));
           setMounted(false);
         }
       })
       .catch((er) => alert(er));
   }, []);
-
-  console.log("Ligand", ligand);
-  console.log(Atoms.atoms?.length);
   useEffect(() => {
     const subscription = Dimensions.addEventListener("change", () => {
       setWidth(Dimensions.get("screen").width);
-      setHeight(Dimensions.get("screen").height);
+      setHeight(Dimensions.get("screen").height - 30);
     });
 
     return () => subscription?.remove();
   }, []);
-
-  useEffect(() => console.log(width, height), [width, height]);
-
+  console.log("LENGTH", Atoms.atoms?.length);
   const geo = new THREE.SphereGeometry();
   const camera = new THREE.PerspectiveCamera(
     75,
@@ -115,101 +60,117 @@ const ViewProtein = (props) => {
   );
   let cameraInitialPositionX = 0;
   let cameraInitialPositionY = 2;
-  let cameraInitialPositionZ = 50;
+  let cameraInitialPositionZ = 500;
+
   return (
     <SafeAreaView style={{ flex: 1, backgroundColor: "#000" }}>
       {mount ? (
         <Text>HIII</Text>
       ) : (
         <>
-          {/* <OrbitControlsView style={{ flex: 1 }} camera={camera}> */}
-          <GLView
-            style={{ width: width, height: height }}
-            onContextCreate={async (gl) => {
-              const { drawingBufferWidth: width, drawingBufferHeight: height } =
-                gl;
+          <OrbitControlsView style={{ flex: 1 }} camera={camera}>
+            <GLView
+              style={{ width: width, height: height }}
+              onContextCreate={async (gl) => {
+                const {
+                  drawingBufferWidth: width,
+                  drawingBufferHeight: height,
+                } = gl;
 
-              const renderer = new Renderer({ gl });
-              renderer.setClearColor("#000");
-              renderer.setSize(width, height);
-
-              camera.position.set(
-                cameraInitialPositionX,
-                cameraInitialPositionY,
-                cameraInitialPositionZ
-              );
-              camera.lookAt(0, 0, 0);
-              const scene = new THREE.Scene();
-
-              const ambientLight = new THREE.DirectionalLight(0xffffff, 0.9);
-              ambientLight.position.set(
-                cameraInitialPositionX,
-                cameraInitialPositionY,
-                cameraInitialPositionZ
-              );
-
-              scene.add(ambientLight);
-
-              /**********Use PDB PARSER */
-              const position = new THREE.Vector3();
-              for (let i = 0; i < Atoms.atoms?.length; i++) {
-                position.x = Atoms.atoms[i].x;
-                position.y = Atoms.atoms[i].y;
-                position.z = Atoms.atoms[i].z;
-
-                let color = new THREE.Color(
-                  "#" + Colors[Atoms.atoms[i].element].jmol
+                const renderer = new Renderer({ gl });
+                renderer.setClearColor("#000");
+                renderer.setSize(width, height);
+                camera.position.set(
+                  cameraInitialPositionX,
+                  cameraInitialPositionY,
+                  cameraInitialPositionZ
                 );
-                const mtrl = new THREE.MeshPhongMaterial({
-                  color: color,
-                  shininess: 50,
-                });
-                let object = new THREE.Mesh(geo, mtrl);
-                position.multiplyScalar(width / height + 1);
-                object.position.copy(position);
-                scene.add(object);
-              }
-              /**************** ADD BOX */
-              const start = new THREE.Vector3();
-              const end = new THREE.Vector3();
-              for (let j = 0; j < connects.length; j++) {
-                for (let i = 1; i < connects[j].length; i++) {
-                  start.x = Atoms.atoms[connects[j][0] - 1].x;
-                  start.y = Atoms.atoms[connects[j][0] - 1].y;
-                  start.z = Atoms.atoms[connects[j][0] - 1].z;
-                  end.x = Atoms.atoms[connects[j][i] - 1]?.x;
-                  end.y = Atoms.atoms[connects[j][i] - 1]?.y;
-                  end.z = Atoms.atoms[connects[j][i] - 1]?.z;
-                  console.log(start, end);
-                  start.multiplyScalar(width / height + 1);
-                  end.multiplyScalar(width / height + 1);
-                  const cylinderGeometry = new THREE.CylinderGeometry();
-                  const object = new THREE.Mesh(
-                    cylinderGeometry,
-                    new THREE.MeshPhongMaterial(0xffffff)
+                // camera.lookAt(0, 0, 0);
+                const scene = new THREE.Scene();
+                console.log(scene.);
+                const ambientLight = new THREE.DirectionalLight(0xffffff, 0.9);
+                ambientLight.position.set(
+                  cameraInitialPositionX,
+                  cameraInitialPositionY,
+                  cameraInitialPositionZ
+                );
+                scene.add(ambientLight);
+                camera.lookAt(
+                  Atoms.atoms[0]?.x,
+                  Atoms.atoms[0]?.y,
+                  Atoms.atoms[0]?.z
+                );
+
+                /**********Use PDB PARSER */
+                const position = new THREE.Vector3();
+                for (let i = 0; i < Atoms.atoms?.length; i++) {
+                  position.x = Atoms.atoms[i].x;
+                  position.y = Atoms.atoms[i].y;
+                  position.z = Atoms.atoms[i].z;
+
+                  let color = new THREE.Color(
+                    "#" + Colors[Atoms.atoms[i].element].jmol
                   );
-                  object.position.copy(start);
-                  object.position.lerp(end, 0.5);
-                  object.scale.set(0.1, 0.1, start.distanceTo(end));
-                  object.position.lerp(end, 0.1);
-                  console.log(object);
-                  object.lookAt(end);
+                  const mtrl = new THREE.MeshPhongMaterial({
+                    color: color,
+                    shininess: 50,
+                  });
+                  let object = new THREE.Mesh(geo, mtrl);
+                  position.multiplyScalar(width / height + 1);
+                  object.position.copy(position);
                   scene.add(object);
                 }
-              }
-              /************** */
+                const start = new THREE.Vector3();
+                const end = new THREE.Vector3();
+                for (let j = 0; j < connects.length; j++) {
+                  for (let i = 1; i < connects[j].length; i++) {
+                    if (connects[j][i] - 1 < Atoms.atoms.length) {
+                      start.x = Atoms.atoms[connects[j][0] - 1].x;
+                      start.y = Atoms.atoms[connects[j][0] - 1].y;
+                      start.z = Atoms.atoms[connects[j][0] - 1].z;
 
-              const render = () => {
-                requestAnimationFrame(render);
-                camera.rotation.y += 0.01;
-                ambientLight.rotation.y += 0.01;
-                renderer.render(scene, camera);
-                gl.endFrameEXP();
-              };
-              render();
-            }}
-          />
-          {/* </OrbitControlsView> */}
+                      end.x = Atoms.atoms[connects[j][i] - 1].x;
+                      end.y = Atoms.atoms[connects[j][i] - 1].y;
+                      end.z = Atoms.atoms[connects[j][i] - 1].z;
+
+                      start.multiplyScalar(width / height + 1);
+                      end.multiplyScalar(width / height + 1);
+                      const geoBox = new THREE.BoxGeometry(
+                        0.5,
+                        0.5,
+                        start.distanceTo(end)
+                      );
+                      const cylinder = new THREE.Mesh(
+                        geoBox,
+                        new THREE.MeshPhongMaterial({ color: 0xffffff })
+                      );
+                      cylinder.position.copy(start);
+                      cylinder.position.lerp(end, 0.5);
+                      cylinder.lookAt(end);
+                      scene.add(cylinder);
+                    }
+                  }
+                }
+
+                /************** */
+
+                const render = () => {
+                  requestAnimationFrame(render);
+                  ambientLight.position.set(
+                    camera.position.x,
+                    camera.position.y,
+                    camera.position.z
+                  );
+
+                  camera.aspect =
+                    width > height ? height / width : width / height;
+                  renderer.render(scene, camera);
+                  gl.endFrameEXP();
+                };
+                render();
+              }}
+            />
+          </OrbitControlsView>
           <View style={{ backgroundColor: "0xFFFFFF" }}>
             <Text style={{ color: "#FFF", fontSize: 16, padding: 5 }}>
               Atom:
