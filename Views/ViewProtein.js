7aa2caa59fffbf7f5205b9d9535089cf3a884fa5
Noreddine Hakkaoui <nhakkaou@e2r5p9.1337.ma>
2022-01-01 22:25:54 +0100

    Fix Error rendering

--- Views/ViewProtein.js
+++ Views/ViewProtein.js
@@ -2,12 +2,20 @@ import { GLView } from "expo-gl";
 import { Renderer } from "expo-three";
 import * as THREE from "three";
 import matchAll from "string.prototype.matchall";
-import React, { useEffect, useState } from "react";
-import { View, Text, SafeAreaView, Dimensions, TouchableOpacity } from "react-native";
-// import { useHistory } from "react-router-native";
-
-import { State, LongPressGestureHandler } from "react-native-gesture-handler";
-import MaterialCommunityIcons from "react-native-vector-icons/MaterialCommunityIcons";
+import React, { useEffect, useState, useRef } from "react";
+import { captureScreen } from "react-native-view-shot";
+import {
+  View,
+  SafeAreaView,
+  Dimensions,
+  ActivityIndicator,
+  StatusBar,
+  Alert,
+  Button,
+} from "react-native";
+import * as MediaLibrary from "expo-media-library";
+import * as Sharing from "expo-sharing";
+import { useHistory } from "react-router-native";
 import Axios from "axios";
 import Colors from "./CPK_Colors.json";
 import { getConnect, mapAtoms } from "../Helpers/PdbParse_Connect";
@@ -15,18 +23,24 @@ import parsePdb from "parse-pdb";
 import OrbitControlsView from "expo-three-orbit-controls";
 
 const ViewProtein = (props) => {
-  // const history = useHistory();
-  let rnd = {};
+  const RefScreen = useRef(null);
+  const history = useHistory();
   const [connects, setConnects] = useState([]);
   const [mount, setMounted] = useState(true);
   const [Atoms, setAtoms] = useState([]);
-  const [atomSelected, setatomSelected] = useState({});
-  // const { ligand } = props.location;
-  const ligand = "10S";
+  const [keyRender, setKeyrender] = useState(false);
+  const [jmol, setJmol] = useState(true);
+  const { ligand } = props.location;
+  console.log(ligand);
   const url1 = `https://files.rcsb.org/ligands/view/${ligand}_model.pdb`;
   const [width, setWidth] = useState(Dimensions.get("screen").width);
   const [height, setHeight] = useState(Dimensions.get("screen").height - 10);
-
+  const [modul, setModulisation] = useState(1);
+  const [aspectRatio, setCameraRatio] = useState(
+    width < height ? width / height : height / width
+  );
+  const [status, requestPermission] = MediaLibrary.usePermissions();
+  let rnd = { render() {} };
   useEffect(() => {
     Axios(url1)
       .then((res) => {
@@ -42,32 +56,26 @@ const ViewProtein = (props) => {
       })
       .catch((er) => alert(er));
   }, []);
-
-  const Zoom = (value) => {
-    if (value && camera.position.z > 16)
-      camera.position.z -= 5
-    if (!value && camera.position.z < 100)
-      camera.position.z += 5
-    camera.lookAt(scene.position)
-    console.log(camera.position.z, camera.position.x, camera.position.y)
-    rnd?.render(scene, camera);
-  }
+  const change_color = () => {
+    setJmol(!jmol);
+    setKeyrender(!keyRender);
+  };
   useEffect(() => {
     const subscription = Dimensions.addEventListener("change", () => {
       setWidth(Dimensions.get("screen").width);
       setHeight(Dimensions.get("screen").height - 30);
+      setCameraRatio(
+        Dimensions.get("screen").width / Dimensions.get("screen").height
+      );
+      setKeyrender(!keyRender);
     });
     return () => subscription?.remove();
-  }, []);
-  console.log("LENGTH", Atoms?.length);
+  }, [width, height]);
+
   const geo = new THREE.SphereGeometry();
+  const geo2 = new THREE.BoxGeometry();
   const scene = new THREE.Scene();
-  const camera = new THREE.PerspectiveCamera(
-    75,
-    width < height ? width / height : height / width,
-    0.1,
-    1000
-  );
+  const camera = new THREE.PerspectiveCamera(75, aspectRatio, 0.1, 1000);
   const raycaster = new THREE.Raycaster();
   const handleStateChange = ({ nativeEvent }) => {
     let mouse = new THREE.Vector2();
@@ -76,54 +84,95 @@ const ViewProtein = (props) => {
     raycaster.setFromCamera(mouse, camera);
     // calculate objects intersecting the picking ray
     const intersects = raycaster.intersectObjects(scene.children);
-    console.log("Selected", mouse.x, mouse.y);
-    if (intersects[0]?.object?.AtomsInfos)
-      // setatomSelected(intersects[0]?.object?.AtomsInfos);
-      alert(intersects[0]?.object?.AtomsInfos.name);
+    if (intersects[0]?.object?.AtomsInfos) {
+      Alert.alert(
+        "Atom Details",
+        `Element : ${intersects[0].object.AtomsInfos.element}
+        Founder by: ${
+          Colors[intersects[0].object.AtomsInfos.element].discoverd_by
+        }
+        phase: ${Colors[intersects[0].object.AtomsInfos.element].phase}`,
+        [
+          {
+            text: "Cancel",
+            onPress: () => console.log("Cancel Pressed"),
+            style: "cancel",
+          },
+          { text: "OK", onPress: () => console.log("OK Pressed") },
+        ],
+        { cancelable: false }
+      );
+    }
   };
-
+  const snapshot = async () => {
+    try {
+      let uri = await captureScreen({
+        format: "jpg",
+        quality: 0.8,
+      });
+      await Sharing.shareAsync(uri, { dialogTitle: "Share this image" });
+      let result = await MediaLibrary.requestPermissionsAsync(true);
+      if (result.status === "granted") {
+        let r = await MediaLibrary.saveToLibraryAsync(uri);
+        console.log(r);
+      }
+    } catch (e) {
+      console.log(e);
+    }
+  };
+  const Zoom = (value) => {
+    if (value) camera.position.z -= 5;
+    if (!value) camera.position.z += 5;
+    camera.lookAt(scene.position);
+    console.log(camera.position.z, camera.position.x, camera.position.y);
+    rnd?.render(scene, camera);
+  };
 
   return (
     <SafeAreaView style={{ flex: 1 }}>
+      <StatusBar animated={true} backgroundColor="#000" />
       {mount ? (
-        <Text>HIII</Text>
+        <ActivityIndicator size="large" />
       ) : (
         <>
-
-
-          <OrbitControlsView style={{ flex: 2 }} camera={camera} onTouchEndCapture={handleStateChange}>
-            {/* <LongPressGestureHandler onHandlerStateChange={handleStateChange} minDurationMs={800}> */}
+          <OrbitControlsView
+            key={keyRender}
+            style={{ flex: 2, position: "relative" }}
+            camera={camera}
+            onTouchEndCapture={handleStateChange}
+          >
             <GLView
+              ref={RefScreen}
+              key={keyRender}
               style={{ width: width, height: height }}
               onContextCreate={async (gl) => {
                 const {
                   drawingBufferWidth: width,
                   drawingBufferHeight: height,
                 } = gl;
-                console.log(width, height, camera.position.z);
                 const renderer = new Renderer({ gl });
                 renderer.setClearColor("#000");
                 renderer.setSize(width, height);
-                rnd = renderer;
                 camera.position.set(0, 2, 60);
                 camera.lookAt(0, 0, 0);
                 const ambientLight = new THREE.DirectionalLight(0xffffff, 0.9);
                 ambientLight.position.copy(camera.position);
                 scene.add(ambientLight);
-                /**********Use PDB PARSER */
+                /********** Use PDB PARSER */
                 const position = new THREE.Vector3();
                 for (let i = 0; i < Atoms?.length; i++) {
+                  let colorCpk = jmol
+                    ? Colors[Atoms[i].element].jmol
+                    : Colors[Atoms[i].element].rasmol;
                   position.x = Atoms[i].x;
                   position.y = Atoms[i].y;
                   position.z = Atoms[i].z;
-                  let color = new THREE.Color(
-                    "#" + Colors[Atoms[i].element].jmol
-                  );
+                  let color = new THREE.Color("#" + colorCpk);
                   const mtrl = new THREE.MeshPhongMaterial({
                     color: color,
                     shininess: 50,
                   });
-                  let object = new THREE.Mesh(geo, mtrl);
+                  let object = new THREE.Mesh(modul === 3 ? geo2 : geo, mtrl);
                   position.multiplyScalar(
                     width > height ? width / height + 1 : height / width + 1
                   );
@@ -133,39 +182,44 @@ const ViewProtein = (props) => {
                 }
                 const start = new THREE.Vector3();
                 const end = new THREE.Vector3();
-                for (let j = 0; j < connects.length; j++) {
-                  for (let i = 1; i < connects[j].length; i++) {
-                    if (connects[j][i] - 1 < Atoms.length) {
-                      start.x = Atoms[connects[j][0] - 1].x;
-                      start.y = Atoms[connects[j][0] - 1].y;
-                      start.z = Atoms[connects[j][0] - 1].z;
-                      end.x = Atoms[connects[j][i] - 1].x;
-                      end.y = Atoms[connects[j][i] - 1].y;
-                      end.z = Atoms[connects[j][i] - 1].z;
+                if (modul !== 2) {
+                  for (let j = 0; j < connects.length; j++) {
+                    for (let i = 1; i < connects[j].length; i++) {
+                      if (connects[j][i] - 1 < Atoms.length) {
+                        start.x = Atoms[connects[j][0] - 1].x;
+                        start.y = Atoms[connects[j][0] - 1].y;
+                        start.z = Atoms[connects[j][0] - 1].z;
+                        end.x = Atoms[connects[j][i] - 1].x;
+                        end.y = Atoms[connects[j][i] - 1].y;
+                        end.z = Atoms[connects[j][i] - 1].z;
 
-                      start.multiplyScalar(
-                        width > height ? width / height + 1 : height / width + 1
-                      );
-                      end.multiplyScalar(
-                        width > height ? width / height + 1 : height / width + 1
-                      );
-                      const geoBox = new THREE.BoxGeometry(
-                        0.5,
-                        0.5,
-                        start.distanceTo(end)
-                      );
-                      const cylinder = new THREE.Mesh(
-                        geoBox,
-                        new THREE.MeshPhongMaterial({ color: 0xffffff })
-                      );
-                      cylinder.position.copy(start);
-                      cylinder.position.lerp(end, 0.5);
-                      cylinder.lookAt(end);
-                      scene.add(cylinder);
+                        start.multiplyScalar(
+                          width > height
+                            ? width / height + 1
+                            : height / width + 1
+                        );
+                        end.multiplyScalar(
+                          width > height
+                            ? width / height + 1
+                            : height / width + 1
+                        );
+                        const geoBox = new THREE.BoxGeometry(
+                          0.5,
+                          0.5,
+                          start.distanceTo(end)
+                        );
+                        const cylinder = new THREE.Mesh(
+                          geoBox,
+                          new THREE.MeshPhongMaterial({ color: 0xffffff })
+                        );
+                        cylinder.position.copy(start);
+                        cylinder.position.lerp(end, 0.5);
+                        cylinder.lookAt(end);
+                        scene.add(cylinder);
+                      }
                     }
                   }
                 }
-
                 /************** */
 
                 const render = () => {
@@ -175,35 +229,69 @@ const ViewProtein = (props) => {
                     camera.position.y,
                     camera.position.z
                   );
-                  console.log(camera.position);
-                  camera.aspect =
-                    width > height ? height / width : width / height;
                   renderer.render(scene, camera);
                   gl.endFrameEXP();
                 };
                 render();
               }}
             />
-            {/* </LongPressGestureHandler> */}
           </OrbitControlsView>
-          <View style={{ backgroundColor: "0xFFFFFF" }}>
-            <TouchableOpacity
-              style={{ backgroundColor: "#fff", color: "#000" }}
-              onPress={() => Zoom(true)}
-            >
-              <Text>+</Text>
-            </TouchableOpacity>
-            <TouchableOpacity
-              style={{ backgroundColor: "#fff", color: "#000" }}
-              onPress={() => Zoom(false)}
-            >
-              <Text>-</Text>
-            </TouchableOpacity>
-            <MaterialCommunityIcons
-              // onPress={() => history.push("/list")}
-              style={{ color: "#fff", padding: 5 }}
-              size={35}
-              name="keyboard-backspace"
+
+          <View
+            style={{
+              padding: 5,
+              width: "30%",
+              backgroundColor: "#fff",
+              position: "absolute",
+              top: 80,
+              right: 10,
+              borderRadius: 10,
+
+              flexDirection: "row",
+            }}
+          >
+            <Button
+              style={{
+                backgroundColor: "#fff",
+                color: "#000",
+              }}
+              title="1"
+              onPress={() => {
+                setModulisation(1);
+                setKeyrender(!keyRender);
+              }}
+            />
+            <Button
+              style={{
+                backgroundColor: "#fff",
+                color: "#000",
+              }}
+              title="2"
+              onPress={() => {
+                setModulisation(2);
+                setKeyrender(!keyRender);
+              }}
+            />
+            <Button
+              style={{
+                backgroundColor: "#fff",
+                color: "#000",
+              }}
+              title="3"
+              onPress={() => {
+                setModulisation(3);
+                setKeyrender(!keyRender);
+              }}
+            />
+            <Button
+              style={{
+                backgroundColor: "#fff",
+                color: "#000",
+              }}
+              title="4"
+              onPress={() => {
+                change_color();
+              }}
             />
           </View>
         </>
