938324cbdf29520ff964f11b3e27aae26eff3e64
Noreddine Hakkaoui <nhakkaou@e2r4p9.1337.ma>
2022-01-03 15:50:16 +0100

    Zoom Work

--- context/auth.js
+++ context/auth.js
@@ -5,46 +5,46 @@ import { navigationRef } from "../navigator/RootNavigation";
 const AuthContext = createContext();
 
 const AuthProvider = ({ children }) => {
-	const [isAuth, setIsAuth] = useState(false);
-	const appState = useRef(AppState.currentState);
+  const [isAuth, setIsAuth] = useState(false);
+  const appState = useRef(AppState.currentState);
 
-	useEffect(() => {
-		if (!isAuth) {
-			navigationRef.current.navigate("Login");
-		}
-	}, [isAuth]);
+  useEffect(() => {
+    if (!isAuth) {
+      navigationRef.current.navigate("Login");
+    }
+  }, [isAuth]);
 
-	changeAuth = (value) => {
-		setIsAuth(value);
-	};
+  changeAuth = (value) => {
+    setIsAuth(value);
+  };
 
-	useEffect(() => {
-		const sub = AppState.addEventListener("change", (nextAppState) => {
-			if (
-				(appState.current === "background" || appState.current === "active") &&
-				nextAppState === "active"
-			) {
-				setIsAuth(false);
-			}
+  useEffect(() => {
+    const sub = AppState.addEventListener("change", (nextAppState) => {
+      if (
+        (appState.current === "background" || appState.current === "active") &&
+        nextAppState === "active"
+      ) {
+        setIsAuth(false);
+      }
 
-			appState.current = nextAppState;
-		});
+      appState.current = nextAppState;
+    });
 
-		return () => {
-			if (sub) sub.remove();
-		};
-	}, []);
+    return () => {
+      if (sub) sub.remove();
+    };
+  }, []);
 
-	return (
-		<AuthContext.Provider
-			value={{
-				isAuth,
-				changeAuth,
-			}}
-		>
-			{children}
-		</AuthContext.Provider>
-	);
+  return (
+    <AuthContext.Provider
+      value={{
+        isAuth,
+        changeAuth,
+      }}
+    >
+      {children}
+    </AuthContext.Provider>
+  );
 };
 
 export { AuthContext, AuthProvider };
